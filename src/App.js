7d8a0782fae0ec9923e25e6415794a0a80a95866
Sarujan Rajaratnam <rajaratnam.sarujan@gmail.com>
2021-03-12 10:10:31 +0100

    Add src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Routes from './config/router'
+import { Online, Offline } from 'react-detect-offline'
+import OffLineChamp from './components/offLineChamp'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Offline>
+        <OffLineChamp />
+      </Offline>
+
+      <Online>
+        <br />
+        <br />
+        <br />
+        <br />
+        <Routes />
+      </Online>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
